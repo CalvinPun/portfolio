c2721088eb52a22183105ea1f6d65245f6df258d
CalvinPun <cp4295@nyu.edu>
2026-03-31 18:11:47 -0400

    added background

--- site/app/page.tsx
+++ site/app/page.tsx
@@ -2,7 +2,7 @@ import { HeroSection } from "@/components/sections/HeroSection";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-[var(--background)] text-stone-900">
+    <main className="min-h-screen text-stone-900">
       <HeroSection />
     </main>
   );
